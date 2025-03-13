daff130bfa8939c656151fd8326169825b1b8ab5
66160375 <160494374+66160375@users.noreply.github.com>
2025-03-13 13:38:28 +0700

    feat(migration) Databate for department

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,10 +6,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-<<<<<<< HEAD
-=======
 use Illuminate\Support\Facades\Auth;
->>>>>>> 454b777bf070994f42c9e946e79df980db1e128e
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
@@ -17,25 +14,9 @@ class LoginController extends Controller
 {
     function index()
     {
-<<<<<<< HEAD
-        return view('login');
-    }
-    function login(Request $req)
-    {
-        //print_r($req->input());
-        $user = User::where('email', $req->email)->first();
-        //print_r($user);
-        if ($user != null && Hash::check($req->password, $user->password)) {
-            $req->session()->put('user', $user);
-            return redirect('/users');
-        } else {
-            $req->session()->flash('error', 'กรุณาตรวจสอบข้อมูลอีกครั้ง');
-            return redirect('/login');
-        }
-=======
         return view("login");  
     }
     function login(Request $req){
         $user = User::where('email',$req->email)->first();
->>>>>>> 454b777bf070994f42c9e946e79df980db1e128e
     }
+}
