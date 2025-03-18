29a221777b7f5678281f454652bbbf08f58f6778
EzekielMorales <basszad3107@gmail.com>
2025-03-18 21:56:08 +0700

    create emp_login
    create emp_login.blade.php

---
+++ resources/views/emp_login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('public\css\style.css') }}">
+    <title>Employee - Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="login-box">
+            <img src="{{ asset('public\image\logo wrs full.png') }}" alt="Website Logo">
+            <!--<h1>Dash UI</h1>-->
+            <p>Please enter your user information.</p>
+            <form action="#" method="post">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Enter your username here" required>
+
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="***************" required>
+
+                <button type="submit">Login</button>
+            </form>
+            <p1>Login for Employees</p1>
+        </div>
+    </div>
+</body>
+</html>
