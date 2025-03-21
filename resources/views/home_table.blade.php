74491d73912a563b9a943e70f8e00fe3ba36aed9
AihMahhh <28punpun@gmail.com>
2025-03-22 03:48:52 +0700

    doc(home-table): เพิ่มคำอธิบายการทำงานของโค้ด

--- resources/views/home_table.blade.php
+++ resources/views/home_table.blade.php
@@ -1,12 +1,21 @@
+{{--
+* employee_task_list.blade.php
+* Display task list for employees and departments
+* @input : -
+* @output : Task list interface with categorized task tabs
+* @author : Pacharathorn Pimngoen 66160235
+* @Create Date : 2025-03-22
+--}}
+
 @extends('layouts.employee_layouts')
 @section('content') 
-
 
 <div class="d-flex">
     <div class="content w-100">
         <h3>รายการงาน</h3>
         <div class="nav-tabs-container">
             <ul class="nav nav-tabs" id="taskTabs">
+                <!-- แท็บสำหรับเลือกประเภทของงาน -->
                 <li class="nav-item">
                     <a class="nav-link active" data-bs-toggle="tab" href="#myTasks">ใบงานของฉัน</a>
                 </li>
@@ -18,6 +27,7 @@
         <div class="tab-content">
             <div class="tab-pane fade show active mt-3" id="myTasks">
                 <ul class="nav nav-tabs">
+                    <!-- แท็บย่อยสำหรับงานของฉัน -->
                     <li class="nav-item">
                         <a class="custom-btn active" data-bs-toggle="tab" href="#received">งานที่ได้รับ</a>
                     </li>
@@ -30,30 +40,33 @@
                 </ul>
                 <div class="tab-content mt-3" style="border:none;">
                     <div class="tab-pane fade show active" id="received">
+                        <!-- ตารางแสดงงานที่ได้รับ -->
                         <table class="table">
                             <thead>
-                                <tr class="table-secondary" >
+                                <tr class="table-secondary">
                                     <th class="col-3" style="padding-left:32px;">ชื่อใบงาน</th>
                                     <th class="col-3">ชื่องาน</th>
                                     <th class="col-2">ผู้มอบหมาย</th>
                                     <th class="col-2">ความสำคัญ</th>
                                     <th class="col-2">กำหนดส่ง</th>
                                 </tr>
-                                <tr class="table" >
+                            </thead>
+                            <tbody>
+                                <!-- ตัวอย่างข้อมูลใบงาน -->
+                                <tr class="table">
                                     <td class="col-3" style="padding-left:32px;">งานของเป้ย</td>
                                     <td class="col-3">ห้องน้ำต้องการตัว</td>
                                     <td class="col-2">ปาล์ม</td>
                                     <td class="col-2">ระดับจักรวาล</td>
                                     <td class="col-2">32/12/2658</td>
                                 </tr>
-                            </thead>
-                            <tbody></tbody>
+                            </tbody>
                         </table>
                     </div>
                     <div class="tab-pane fade" id="inprogress">
                         <table class="table">
                             <thead>
-                                <tr class="table-secondary" >
+                                <tr class="table-secondary">
                                     <th class="col-3" style="padding-left:32px;">ชื่อใบงาน</th>
                                     <th class="col-3">ชื่องาน</th>
                                     <th class="col-2">ผู้มอบหมาย</th>
@@ -67,7 +80,7 @@
                     <div class="tab-pane fade" id="completed">
                         <table class="table">
                             <thead>
-                                <tr class="table-secondary" >
+                                <tr class="table-secondary">
                                     <th class="col-3" style="padding-left:32px;">ชื่อใบงาน</th>
                                     <th class="col-3">ชื่องาน</th>
                                     <th class="col-2">ผู้มอบหมาย</th>
@@ -82,6 +95,7 @@
             </div>
             <div class="tab-pane fade mt-3" id="departmentTasks">
                 <ul class="nav nav-tabs">
+                    <!-- แท็บย่อยสำหรับใบงานของแผนก -->
                     <li class="nav-item">
                         <a class="custom-btn active" data-bs-toggle="tab" href="#deptReceived">งานที่ได้รับ</a>
                     </li>
@@ -96,7 +110,7 @@
                     <div class="tab-pane fade show active" id="deptReceived">
                         <table class="table">
                             <thead>
-                                <tr class="table-secondary" >
+                                <tr class="table-secondary">
                                     <th class="col-3" style="padding-left:32px;">ชื่อใบงาน</th>
                                     <th class="col-3">ชื่องาน</th>
                                     <th class="col-2">ผู้มอบหมาย</th>
@@ -110,7 +124,7 @@
                     <div class="tab-pane fade" id="deptInprogress">
                         <table class="table">
                             <thead>
-                                <tr class="table-secondary" >
+                                <tr class="table-secondary">
                                     <th class="col-3" style="padding-left:32px;">ชื่อใบงาน</th>
                                     <th class="col-3">ชื่องาน</th>
                                     <th class="col-2">ผู้มอบหมาย</th>
@@ -124,7 +138,7 @@
                     <div class="tab-pane fade" id="deptCompleted">
                         <table class="table">
                             <thead>
-                                <tr class="table-secondary" >
+                                <tr class="table-secondary">
                                     <th class="col-3" style="padding-left:32px;">ชื่อใบงาน</th>
                                     <th class="col-3">ชื่องาน</th>
                                     <th class="col-2">ผู้มอบหมาย</th>
