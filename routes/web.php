86e31b2a14603e9619bae709d4763cc0a89424c7
Natthanan04 <natthanansirisurayut@gmail.com>
2025-03-29 22:24:22 +0700

    fix(side_bar_admin.blade.php) : แก้ไข active ที่ค้างอยู่ในเมนูที่ไม่ได้เลือก

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,8 @@ Route::get('/manage_department', function () {
     return view('admin.manage_department');
 });
 
-Route::get('/form', function () {
-    return view('create_form');
-})->name('create-form');
 
-// ทดลองใส่หน้าแรกเฉยๆ
-Route::get('/main', function () {
-    return view('create_form');
-})->name('main-page');
+
+Route::get('/manage_department', function () {
+    return view('admin.manage_department');
+})->name('manage-department');
