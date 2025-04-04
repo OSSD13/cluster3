c7bda1b6a89f9df0927ab1bbd987eeb4615090ff
Natthanan04 <natthanansirisurayut@gmail.com>
2025-04-04 20:05:20 +0700

    fix(web.php) : ลบ route ที่ไม่ได้ใช้

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SidebarController;
 use App\Http\Controllers\DepartmentController;
 
-
-Route::get('/manage_department', function () {
-    return view('admin.manage_department');
-});
-
-
 Route::get('/department', [DepartmentController::class, 'showDepartments'])->name('manage-department');
 Route::post('/department', [DepartmentController::class, 'createDepartment'])->name('department.createDepartment');
 Route::put('/department/{id}', [DepartmentController::class, 'updateDepartment'])->name('department.updateDepartment');
