eb68edb7f7136c6f52ac28b1b8982bbee5c6083f
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-04-05 16:24:27 +0700

    stype(employee_model):
    -เเก้ไขให้เหมือนกับ figma มากขึ้น

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ManageEmployeeControler;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ManageEmployeeControler::class, 'showEmployee'])->name('manage_employee.showEmployees');
 Route::get('/manage_employee', [ManageEmployeeControler::class, 'showEmployee'])->name('manage_employee.showEmployees');
 Route::put('/edit/{id}', [ManageEmployeeControler::class, 'editEmployee'])->name('manage_employee_edit');
 Route::get('/search_employee', [ManageEmployeeControler::class, 'searchEmployee'])->name('manage_employee_search');
