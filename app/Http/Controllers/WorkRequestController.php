1401b079aaf92db8d5619f8cbc31d5801f4faecb
66160375 <160494374+66160375@users.noreply.github.com>
2025-04-06 10:26:14 +0700

    stye(home_table_blade.php)"ปรับโค้ด"

---
+++ app/Http/Controllers/WorkRequestController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Department;
+use App\Models\Employee;
+use App\Models\Task;
+use App\Models\WorkRequest;
+use Illuminate\Http\Request;
+
+class WorkRequestController extends Controller
+{
+    // public function showDetail()
+    // {
+    //     $emp = Employee::all();
+    //     $workRequest = WorkRequest::all();
+    //     $task = Task::all();
+    //     $dept = Department::all();
+    //     return view('home_show_detail', compact('emp', 'workRequest', 'task', 'dept'));
+    // }
+
+    // public function archive()
+    // {
+    //     // ดึงข้อมูลงานที่เสร็จแล้วและถูกปฏิเสธ
+    //     $completedRequests = WorkRequest::where('req_status', 'Completed')->get();
+    //     $rejectedRequests = WorkRequest::where('req_status', 'Rejected')->get();
+    //     $completedTasks = Task::where('tsk_status', 'Completed')->get();
+    //     $rejectedTasks = Task::where('tsk_status', 'Rejected')->get();
+    //     $tasks = Task::with('workRequest.tasks')->get();
+    //     $workRequests = WorkRequest::with(['employee', 'department', 'tasks'])->get();
+    //     // ส่งข้อมูลไปยัง view
+    //     return view('archive_table', compact('completedRequests', 'rejectedRequests','completedTasks', 'rejectedTasks','tasks','workRequests'));
+    // }
+
+    // public function sent()
+    // {
+    //     return view('sent');
+    // }
+
+        public function main()
+    {
+        $emp = Employee::all();
+        $workRequest = WorkRequest::all();
+        $task = Task::all();
+        $dept = Department::all();
+        return view('home_table', compact('emp', 'workRequest', 'task', 'dept'));
+    }
+}
