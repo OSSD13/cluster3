0872cc1d06ed268cddb9770ff68e1d5a3688de06
Natthanan04 <natthanansirisurayut@gmail.com>
2025-04-07 09:12:43 +0700

    feat(AppServiceProvider) : add boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $host = request()->getHost();
+
+    if (str_contains($host, 'se.buu.ac.th')) {
+        // ถ้าเข้าโดเมนมหาลัย
+        URL::forceRootUrl(config('app.url'));
+        URL::forceScheme('https');
+        config(['app.asset_url' => '/cluster3']); // กำหนด /cluster3 สำหรับโดเมน
+    } else {
+        // กรณีเข้า IP
+        URL::forceRootUrl('http://' . $host . ':1303');  // ใช้ IP address
+        config(['app.asset_url' => null]); // ไม่มี /cluster3 สำหรับ IP
+    }
     }
 }
