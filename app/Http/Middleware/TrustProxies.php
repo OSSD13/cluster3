008ee0f515bde7b6eb499f865a7bb382bb4452dc
Natthanan04 <natthanansirisurayut@gmail.com>
2025-04-07 09:46:07 +0700

    feat(TrustProxies) : add file

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrustProxies
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request);
+    }
+    protected $proxies = '*'; // หรือใส่ IP ที่ใช้ reverse proxy ก็ได้
+
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
+}
