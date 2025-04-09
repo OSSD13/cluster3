2e11200a3cd98ac83370adebe7a53d8609fd10b0
Natthanan04 <natthanansirisurayut@gmail.com>
2025-04-09 16:55:32 +0700

    fix(css) : fix font size

--- resources/views/layouts/employee_layouts.blade.php
+++ resources/views/layouts/employee_layouts.blade.php
@@ -65,7 +65,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/monthSelect/style.css">
 
     {{-- JavaScript Section --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -81,6 +81,7 @@
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
 
 
     <style>
